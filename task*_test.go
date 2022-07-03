7f7c83c5e2237be6556ce836599eb956fe21c734
mdalbrid <k1_2-d@mail.ru>
2022-07-04 00:21:48 +0300

    finish

---
+++ task*_test.go
@@ -0,0 +1,30 @@
+package homework
+
+//func TestAverage(t *testing.T) {
+//	v := average([6]float32{1, 2, 3, 4, 5, 6})
+//
+//	if v != 3.5 {
+//		t.Error("Expected 3.5, got ", v)
+//	}
+//
+//}
+
+//func TestReverse(t *testing.T) {
+//	v := reverse([]int64{1, 2, 5, 15})
+//	if ok := reflect.DeepEqual(v, []int64{15, 5, 2, 1}); !ok {
+//		t.Error(v, ok)
+//	}
+//}
+
+//func TestSortMapValues(t *testing.T) {
+//	v := sortMapValues(map[int]string{2: "a", 0: "b", 1: "c"})
+//	v2 := sortMapValues(map[int]string{10: "aa", 0: "bb", 500: "cc"})
+//
+//	fmt.Println(v, v2)
+//	if ok := reflect.DeepEqual(v, []string{"b", "c", "a"}); !ok {
+//		t.Error(v, ok)
+//	}
+//	if ok := reflect.DeepEqual(v2, []string{"bb", "aa", "cc"}); !ok {
+//		t.Error(v, ok)
+//	}
+//}
--- task01-arrays.go
+++ task01-arrays.go
@@ -2,5 +2,13 @@ package homework
 
 func average(input [15]float32) (result float32) {
 	//Place your code here
+
+	var sum float32
+
+	for i := range input {
+		sum += input[i]
+	}
+	result = sum / float32(len(input))
+
 	return
 }
